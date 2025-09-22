370f1493c2f06fd667ee9c5853210be6bdee6d23
ra2607Alessandro <2alessandro.aimar@gmail.com>
2025-09-22 09:50:07 +0200

    almost there

--- index.jsx
+++ index.jsx
@@ -1,3 +1,5 @@
+const { type } = require("os")
+
 const Didact = {createElement, render}
 
 
@@ -41,8 +43,75 @@ function createDOM(fiber) {
     container.appendChild(dom)
  }
 
-function commitRoot() {
 
+const isEvent = key => key.startsWith("on")
+const isProperty = key => key !== "children" && !isEvent(key)
+const isNew = (prev, next) => key => !(key in next)
+
+function updateDom(dom, prevProps, nextProps) {
+    Object.keys(prevProps)
+    .filter(isEvent)
+    .filter(
+        key => 
+            !(key in nextProps) ||
+        isNew(prevProps, nextProps)(key)
+    )
+    .forEach(name => {
+        const eventType = name
+        .toLowerCase()
+        .substring(2)
+
+        dom.removeChildListener(
+            eventType, 
+            prevProps[name]
+        )
+    })
+}
+function commitRoot() {
+    deletions.forEach(commitWork)
+    commitWork(wipRoot.child)
+    currentRoot = wipRoot
+    wipRoot = null
+
+}
+
+function commitWork(fiber) {
+    
+
+    if (!fiber) {
+        return
+    }
+    let domParentFiber = fiber.parent
+    while (!domParentFiber.dom) {
+        domParentFiber = domParentFiber.parent
+    }
+    const domParent = fiber.parent.dom 
+    if (
+        fiber.effectTag === "PLACEMENT" && fiber.dom != null
+    )
+    { 
+    domParent.appendChild(fiber.dom)}
+    else if (fiber.effectTag === "UPDATE" && fiber.dom != null) {
+        updateDom(
+            fiber.dom,
+            fiber.alternate.props,
+            fiber.props
+        )
+    }
+    else if (fiber.effectTag === "DELETION") {
+        commitDeletion(fiber, domParent)
+    }
+    commitWork(fiber.child)
+    commitWork(fiber.sibling)
+
+}
+
+function commitDeletion(fiber, domParent){
+    if(fiber.dom){
+        deletions.removeChild(fiber.dom)
+    } else {
+        commitDeletion(fiber.child, domParent)
+    }
 }
 
 
@@ -52,8 +121,10 @@ function render(element, container ) {
         props: {
             children: [element],
         },
+        alternate : currentRoot
     }
-
+    
+    deletions = []
     nextUnitOfWork= wipRoot
 
     const dom = 
@@ -72,6 +143,8 @@ function render(element, container ) {
  }
 let nextUnitOfWork = null
 let wipRoot = null 
+let currentRoot = null
+let deletions = null
 
 function workLoop(deadline) {
     let shouldYield = false
@@ -89,7 +162,13 @@ function workLoop(deadline) {
 
 requestIdCallback(workLoop)
 
-function performUnitOfWork(nextUnitOfWork) {
+function performUnitOfWork(fiber) {
+    const isFunctionComponent = 
+    fiber.type instanceof Function
+
+    if (isFunctionComponent) {
+        updateFunctionComponent(fiber)
+    }
     // search for a fiber or create one
     if (!fiber.dom){
         fiber.dom = createDOM(fiber)
@@ -143,4 +222,64 @@ function performUnitOfWork(nextUnitOfWork) {
         nextFiber = nextFiber.parent
     }
 
+}
+
+function updateFunctionComponent(fiber) {
+    const children = [fiber.type (fiber.props)]
+    reconcileChildren(fiber, children)
+
+}
+
+function updateHostComponent(fiber) {
+    if (!fiber.dom) {
+        fiber.dom = createDOM(fiber)
+    }
+    reconcileChildren(fiber, fiber.props, children)
+}
+
+function reconcileChildren(wipFiber, elements) {
+    let index = 0
+    let wipFiber =
+    wipFiber && wipFiber.alternate.child
+    let prevSibling = null
+
+    while (
+        index < elements.length || oldFiber != null
+    ) {
+        const element = elements[index]
+        let newFiber = null
+    }
+
+    //We start comparing the old fiber to the element 
+
+    const Sametype =
+       oldFiber &&
+       element &&
+       element.type == oldFiber.type
+
+       if (Sametype) {
+        newFiber = {
+          type : oldFiber.type,
+          props : element.props,
+          dom : oldFiber.dom,
+          parent: wipFiber,
+          alternate: oldFiber,
+          effectTag: "UPDATE"
+        }
+       }
+
+       if ( element && !Sametype) {
+        newFiber = {
+            type : element.type,
+            props: element.props,
+            dom: null,
+            parent: wipFiber,
+            alternate: null,
+            effectTag: "PLACEMENT"
+        }
+       }
+       if (oldFiber && !Sametype) {
+        oldFiber.effectTag = "DELETE"
+        deletion.push(oldFiber)
+       }
 }
